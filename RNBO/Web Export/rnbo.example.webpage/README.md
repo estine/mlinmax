827d93c029bf463bf663cad6d6a181f0de1d860f
Eli Stine <stine.eli@gmail.com>
2023-03-04 17:05:28 -0500

    Add web export RNBO files

---
+++ RNBO/Web Export/rnbo.example.webpage/README.md
@@ -0,0 +1,114 @@
+# RNBO Webpage Example
+
+This example shows you how to add dynamic audio to a webpage, using the web export feature of RNBO, part of [Max 8](https://cycling74.com/products/max) made by [Cycling '74](https://cycling74.com).
+
+This repository uses [Node](https://nodejs.org/en/) to launch a simple web server to make your webpage available locally. For more on why this is necessary, see [Why a local server?](#why-a-local-server)
+
+## Prerequisites
+
+In order to run this example, you'll need `node`, `npm`, and access to the command line. The `npx` binary ships with `node`, so just download and install that from the [Node.js downloads site](https://nodejs.org/en/download/). The recommended version is their latest `LTS`, which at the time of writing this document is version 16.
+
+If have heard about `node` and `npm` before but would like to know more about the included `npx` Package Runner please refer to the [Node.js Documentation](https://nodejs.dev/learn/the-npx-nodejs-package-runner).
+
+## File structure
+
+The source code of the web application is in the `js/` directory. This directory contains the file `app.js`, which does all the work of loading and connecting your RNBO patch. There is also a file `guardrails.js`, which simply tries to provide some clear feedback if you're not running this example in the intended way.
+
+Some notable files/directories:
+
+| Location                          | Explanation   |
+| --------------------------------- | ------------- |
+| export/                           | The directory into which you should export your RNBO code |
+| js/                               | Source for the project, edit it however you like |
+| index.html                        | The web page itself |
+
+## Using this Template
+
+This Github repo is a template, which means you can use it to start your own git-based project using this repository as a starting point. The major difference between a template and a fork is that your new project won't include the commit history of this template--it will be an entirely new starting point. For more see [the official description](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template).
+
+### Getting Started
+
+To get started, first create a new repository to hold your project using this repository as a template. If you're viewing this repo on Github, you should see a button at the top of the page that says `Use this template`. 
+
+![Use this template button](./img/use-this-template-button.png)
+
+You can also follow [the official steps](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) on Github for creating a new repository from a template.
+
+Now you need to copy this repository locally. Follow [the official steps](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) to clone your repository.
+
+### Working with RNBO
+
+Next, open the RNBO patcher you'd like to work with, and navigate to the export sidebar. Find the "Web Export" target.
+
+![Web export in the sidebar](./img/js-export-location.png)
+
+Export your project, making sure to export into the `export` folder in your repository directory. Your export directory should look something like this:
+
+```
+export/
+├─ patch.export.json
+├─ README.md
+```
+
+Whenever you make a change to your RNBO patch, remember to export the source code again to update this file. Now that you've exported your RNBO code, we're ready to open the webpage. From the repository root, run the following command to start the local web server
+
+```sh
+npx http-server
+```
+
+When asked to install the `http-server` in order to proceed simply agree by using `y`. You will only have to agree on that once.
+Once the server started up successfully you may see something like the following in the console:
+
+```sh
+Available on:
+  http://127.0.0.1:8080
+  http://192.168.88.139:8080
+Hit CTRL-C to stop the server
+```
+Open the shown URL, fe. `http://127.0.0.1:8080` in your default browser, if everything went well, you should see and hear your RNBO patch.
+
+### Exporting a new patch
+
+This example looks in the `export` directory for a patch named `patch.export.json`. If you change the name of your export to something other than `patch.export.json`, you'll need to change the JavaScript as well. In `js/app.js`, the line:
+
+```js
+const patchExportURL = "export/patch.export.json";
+```
+
+can be changed to reflect the name of your export.
+
+## Troubleshooting
+
+### Why don't I see anything?
+
+First, check your developer console. On MacOS, you can bring this up in most browsers by pressing Command-Option-I on a Mac. Firefox puts developer tools under Tools > Browser Tools > Web Developer Tools. Other browsers may put this feature somewhere else, so check the documentation for your browser of choice. The important thing to do here is to make sure you don't see any error. If you see something in red, read the message carefully.
+
+### Something doesn't seem to be working right
+
+It might be that the version of RNBO that you used to export your patch doesn't match the version of the RNBO library that `index.html` is downloading. Look for a message in the developer console talking about mismatched versions. To fix this, either export a version of your patch using a more up-to-date version of RNBO, or else change the `script` tags in `index.html` to download a different version of the RNBO libraries.
+
+### My samples aren't loading correctly
+
+Again check the developer console, this time looking for error messages about a failure to decode audio data. Some browsers, like Chrome for example, don't support decoding `.aif` files. So if you're using `anton.aif` as a sample dependency, you should export again using `anton.wav`. Or maybe find another sample to use.
+
+### Why isn't my patch changing in the browser?
+
+If you changed your exported patch in the `export` folder but your patch isn't changing in the browser, you might need to hard refresh the page (cmd+shift+R). This clears the cache to account for any changes to the page being served.
+
+## Why a local server?
+We're recreating on a very small scale what happens whenever you load a website on your computer. When you run `npx http-server`, a Node process starts. This process binds to a port on your machine, defaulting to port 8080. When your browser tries to access the website `http://localhost:8080`, it connects to the server and tries to get the content for the given path, which is `/`. Given this path, the server returns the contents of the file `index.html`, which is what you see when you load the page.
+
+As part of loading that page, your web browser also asks the server for the JavaScript file at `js/app.js`. When the browser executes this script, it makes yet another request to fetch the file at the path `export/patch.export.json`. Finally, the script can use this exported patch to create a RNBO JavaScript object and connect it to the audio graph in the current page.
+
+The important takeaway here is that this is the kind of interaction that your browser is expecting: making HTTP and HTTPS requests to fetch resources from a remote server. It's technically possible to simply double-click on the `index.html` file and to load the page using the `file:` protocol instead of `http:` or `https:`. However, for security reasons this will block access to `WebAssembly` or `AudioWorklets`, which will keep our exported RNBO patch from working the way we want. Running a local server lets the browser treat the webpage as if it were pulled from the internet like any other page.
+
+The other reason that we run a server this way is because this brings us much closer to putting our RNBO patch on the publically accessible internet. If you want to build a public website containing a RNBO patch, it's helpful to keep this simple example in mind when you think about what resources to put where.
+
+## Customizing your web page
+
+From this point, the sky is the limit. You can do anything and everything to your web page, adding custom graphics and interaction in whatever way you like. A full discussion of web programming is beyond the scope of this README, but some useful reading material would include:
+
+- [ReactJS](https://reactjs.org/)
+- [p5JS](https://p5js.org/)
+- [Canvas API](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial)
+- [Netlify](https://www.netlify.com/)
---
+++ RNBO/Web Export/rnbo.example.webpage/js/app.js
@@ -0,0 +1,339 @@
+async function setup() {
+    const patchExportURL = "export/patch.export.json";
+
+    // Create AudioContext
+    const WAContext = window.AudioContext || window.webkitAudioContext;
+    const context = new WAContext();
+
+    // Create gain node and connect it to audio output
+    const outputNode = context.createGain();
+    outputNode.connect(context.destination);
+    
+    // Fetch the exported patcher
+    let response, patcher;
+    try {
+        response = await fetch(patchExportURL);
+        patcher = await response.json();
+    
+        if (!window.RNBO) {
+            // Load RNBO script dynamically
+            // Note that you can skip this by knowing the RNBO version of your patch
+            // beforehand and just include it using a <script> tag
+            await loadRNBOScript(patcher.desc.meta.rnboversion);
+        }
+
+    } catch (err) {
+        const errorContext = {
+            error: err
+        };
+        if (response && (response.status >= 300 || response.status < 200)) {
+            errorContext.header = `Couldn't load patcher export bundle`,
+            errorContext.description = `Check app.js to see what file it's trying to load. Currently it's` +
+            ` trying to load "${patchExportURL}". If that doesn't` + 
+            ` match the name of the file you exported from RNBO, modify` + 
+            ` patchExportURL in app.js.`;
+        }
+        if (typeof guardrails === "function") {
+            guardrails(errorContext);
+        } else {
+            throw err;
+        }
+        return;
+    }
+    
+    // (Optional) Fetch the dependencies
+    let dependencies = [];
+    try {
+        const dependenciesResponse = await fetch("export/dependencies.json");
+        dependencies = await dependenciesResponse.json();
+
+        // Prepend "export" to any file dependenciies
+        dependencies = dependencies.map(d => d.file ? Object.assign({}, d, { file: "export/" + d.file }) : d);
+    } catch (e) {}
+
+    // Create the device
+    let device;
+    try {
+        device = await RNBO.createDevice({ context, patcher });
+    } catch (err) {
+        if (typeof guardrails === "function") {
+            guardrails({ error: err });
+        } else {
+            throw err;
+        }
+        return;
+    }
+
+    // (Optional) Load the samples
+    if (dependencies.length)
+        await device.loadDataBufferDependencies(dependencies);
+
+    // Connect the device to the web audio graph
+    device.node.connect(outputNode);
+
+    // (Optional) Extract the name and rnbo version of the patcher from the description
+    document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher") + " (v" + patcher.desc.meta.rnboversion + ")";
+
+    // (Optional) Automatically create sliders for the device parameters
+    makeSliders(device);
+
+    // (Optional) Create a form to send messages to RNBO inputs
+    makeInportForm(device);
+
+    // (Optional) Attach listeners to outports so you can log messages from the RNBO patcher
+    attachOutports(device);
+
+    // (Optional) Load presets, if any
+    loadPresets(device, patcher);
+
+    // (Optional) Connect MIDI inputs
+    makeMIDIKeyboard(device);
+
+    document.body.onclick = () => {
+        context.resume();
+    }
+
+    // Skip if you're not using guardrails.js
+    if (typeof guardrails === "function")
+        guardrails();
+}
+
+function loadRNBOScript(version) {
+    return new Promise((resolve, reject) => {
+        if (/^\d+\.\d+\.\d+-dev$/.test(version)) {
+            throw new Error("Patcher exported with a Debug Version!\nPlease specify the correct RNBO version to use in the code.");
+        }
+        const el = document.createElement("script");
+        el.src = "https://c74-public.nyc3.digitaloceanspaces.com/rnbo/" + encodeURIComponent(version) + "/rnbo.min.js";
+        el.onload = resolve;
+        el.onerror = function(err) {
+            console.log(err);
+            reject(new Error("Failed to load rnbo.js v" + version));
+        };
+        document.body.append(el);
+    });
+}
+
+function makeSliders(device) {
+    let pdiv = document.getElementById("rnbo-parameter-sliders");
+    let noParamLabel = document.getElementById("no-param-label");
+    if (noParamLabel && device.numParameters > 0) pdiv.removeChild(noParamLabel);
+
+    // This will allow us to ignore parameter update events while dragging the slider.
+    let isDraggingSlider = false;
+    let uiElements = {};
+
+    device.parameters.forEach(param => {
+        // Subpatchers also have params. If we want to expose top-level
+        // params only, the best way to determine if a parameter is top level
+        // or not is to exclude parameters with a '/' in them.
+        // You can uncomment the following line if you don't want to include subpatcher params
+        
+        //if (param.id.includes("/")) return;
+
+        // Create a label, an input slider and a value display
+        let label = document.createElement("label");
+        let slider = document.createElement("input");
+        let text = document.createElement("input");
+        let sliderContainer = document.createElement("div");
+        sliderContainer.appendChild(label);
+        sliderContainer.appendChild(slider);
+        sliderContainer.appendChild(text);
+
+        // Add a name for the label
+        label.setAttribute("name", param.name);
+        label.setAttribute("for", param.name);
+        label.setAttribute("class", "param-label");
+        label.textContent = `${param.name}: `;
+
+        // Make each slider reflect its parameter
+        slider.setAttribute("type", "range");
+        slider.setAttribute("class", "param-slider");
+        slider.setAttribute("id", param.id);
+        slider.setAttribute("name", param.name);
+        slider.setAttribute("min", param.min);
+        slider.setAttribute("max", param.max);
+        if (param.steps > 1) {
+            slider.setAttribute("step", (param.max - param.min) / (param.steps - 1));
+        } else {
+            slider.setAttribute("step", (param.max - param.min) / 1000.0);
+        }
+        slider.setAttribute("value", param.value);
+
+        // Make a settable text input display for the value
+        text.setAttribute("value", param.value.toFixed(1));
+        text.setAttribute("type", "text");
+
+        // Make each slider control its parameter
+        slider.addEventListener("pointerdown", () => {
+            isDraggingSlider = true;
+        });
+        slider.addEventListener("pointerup", () => {
+            isDraggingSlider = false;
+            slider.value = param.value;
+            text.value = param.value.toFixed(1);
+        });
+        slider.addEventListener("input", () => {
+            let value = Number.parseFloat(slider.value);
+            param.value = value;
+        });
+
+        // Make the text box input control the parameter value as well
+        text.addEventListener("keydown", (ev) => {
+            if (ev.key === "Enter") {
+                let newValue = Number.parseFloat(text.value);
+                if (isNaN(newValue)) {
+                    text.value = param.value;
+                } else {
+                    newValue = Math.min(newValue, param.max);
+                    newValue = Math.max(newValue, param.min);
+                    text.value = newValue;
+                    param.value = newValue;
+                }
+            }
+        });
+
+        // Store the slider and text by name so we can access them later
+        uiElements[param.id] = { slider, text };
+
+        // Add the slider element
+        pdiv.appendChild(sliderContainer);
+    });
+
+    // Listen to parameter changes from the device
+    device.parameterChangeEvent.subscribe(param => {
+        if (!isDraggingSlider)
+            uiElements[param.id].slider.value = param.value;
+        uiElements[param.id].text.value = param.value.toFixed(1);
+    });
+}
+
+function makeInportForm(device) {
+    const idiv = document.getElementById("rnbo-inports");
+    const inportSelect = document.getElementById("inport-select");
+    const inportText = document.getElementById("inport-text");
+    const inportForm = document.getElementById("inport-form");
+    let inportTag = null;
+    
+    // Device messages correspond to inlets/outlets or inports/outports
+    // You can filter for one or the other using the "type" of the message
+    const messages = device.messages;
+    const inports = messages.filter(message => message.type === RNBO.MessagePortType.Inport);
+
+    if (inports.length === 0) {
+        idiv.removeChild(document.getElementById("inport-form"));
+        return;
+    } else {
+        idiv.removeChild(document.getElementById("no-inports-label"));
+        inports.forEach(inport => {
+            const option = document.createElement("option");
+            option.innerText = inport.tag;
+            inportSelect.appendChild(option);
+        });
+        inportSelect.onchange = () => inportTag = inportSelect.value;
+        inportTag = inportSelect.value;
+
+        inportForm.onsubmit = (ev) => {
+            // Do this or else the page will reload
+            ev.preventDefault();
+
+            // Turn the text into a list of numbers (RNBO messages must be numbers, not text)
+            const values = inportText.value.split(/\s+/).map(s => parseFloat(s));
+            
+            // Send the message event to the RNBO device
+            let messageEvent = new RNBO.MessageEvent(RNBO.TimeNow, inportTag, values);
+            device.scheduleEvent(messageEvent);
+        }
+    }
+}
+
+function attachOutports(device) {
+    const outports = device.outports;
+    if (outports.length < 1) {
+        document.getElementById("rnbo-console").removeChild(document.getElementById("rnbo-console-div"));
+        return;
+    }
+
+    document.getElementById("rnbo-console").removeChild(document.getElementById("no-outports-label"));
+    device.messageEvent.subscribe((ev) => {
+
+        // Ignore message events that don't belong to an outport
+        if (outports.findIndex(elt => elt.tag === ev.tag) < 0) return;
+
+        // Message events have a tag as well as a payload
+        console.log(`${ev.tag}: ${ev.payload}`);
+
+        document.getElementById("rnbo-console-readout").innerText = `${ev.tag}: ${ev.payload}`;
+    });
+}
+
+function loadPresets(device, patcher) {
+    let presets = patcher.presets || [];
+    if (presets.length < 1) {
+        document.getElementById("rnbo-presets").removeChild(document.getElementById("preset-select"));
+        return;
+    }
+
+    document.getElementById("rnbo-presets").removeChild(document.getElementById("no-presets-label"));
+    let presetSelect = document.getElementById("preset-select");
+    presets.forEach((preset, index) => {
+        const option = document.createElement("option");
+        option.innerText = preset.name;
+        option.value = index;
+        presetSelect.appendChild(option);
+    });
+    presetSelect.onchange = () => device.setPreset(presets[presetSelect.value].preset);
+}
+
+function makeMIDIKeyboard(device) {
+    let mdiv = document.getElementById("rnbo-clickable-keyboard");
+    if (device.numMIDIInputPorts === 0) return;
+
+    mdiv.removeChild(document.getElementById("no-midi-label"));
+
+    const midiNotes = [49, 52, 56, 63];
+    midiNotes.forEach(note => {
+        const key = document.createElement("div");
+        const label = document.createElement("p");
+        label.textContent = note;
+        key.appendChild(label);
+        key.addEventListener("pointerdown", () => {
+            let midiChannel = 0;
+
+            // Format a MIDI message paylaod, this constructs a MIDI on event
+            let noteOnMessage = [
+                144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
+                note, // MIDI Note
+                100 // MIDI Velocity
+            ];
+        
+            let noteOffMessage = [
+                128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
+                note, // MIDI Note
+                0 // MIDI Velocity
+            ];
+        
+            // Including rnbo.min.js (or the unminified rnbo.js) will add the RNBO object
+            // to the global namespace. This includes the TimeNow constant as well as
+            // the MIDIEvent constructor.
+            let midiPort = 0;
+            let noteDurationMs = 250;
+        
+            // When scheduling an event to occur in the future, use the current audio context time
+            // multiplied by 1000 (converting seconds to milliseconds) for now.
+            let noteOnEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000, midiPort, noteOnMessage);
+            let noteOffEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000 + noteDurationMs, midiPort, noteOffMessage);
+        
+            device.scheduleEvent(noteOnEvent);
+            device.scheduleEvent(noteOffEvent);
+
+            key.classList.add("clicked");
+        });
+
+        key.addEventListener("pointerup", () => key.classList.remove("clicked"));
+
+        mdiv.appendChild(key);
+    });
+}
+
+setup();
